29d0c1dc8e5b4cd48edacc21d54df04d5a5ceca9
MukeshMr06 <mukeshmr0602@gmail.com>
2025-07-12 10:29:51 -0500

    upload all files

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../Component/Hero'
+import FeatureDestination from '../Component/FeatureDestination'
+import ExclusiveOffer from '../Component/ExclusiveOffer'
+import Testimonial from '../Component/Testimonial'
+import Newsletter from '../Component/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeatureDestination />
+      <ExclusiveOffer />
+      <Testimonial />
+      <Newsletter />
+    </>
+  )
+}
+
+export default Home
